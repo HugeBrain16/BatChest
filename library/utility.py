2ffa2d015208c71fc770c8a54165b67e3f03185d
HugeBrain16 <joshtuck373@gmail.com>
2021-11-22 21:24:59 +0900

    add command loader utility

--- library/utility.py
+++ library/utility.py
@@ -1,5 +1,11 @@
 """utility library for the bot"""
 
+import os
+import importlib
+from typing import List
+from cmdtools.ext.command import CommandWrapper
+
+
 def mention_to_id(mention: str) -> int:
     """extract user id from discord mention"""
     result = 0
@@ -12,3 +18,30 @@ def mention_to_id(mention: str) -> int:
         pass
 
     return result
+
+
+def load_command(name: str):
+    """load commands from library"""
+    for file in os.listdir("library/commands"):
+        if file.endswith(".py") and os.path.isfile("library/commands/" + file):
+            modname = file.rsplit(".py", 1)[0]
+            modpath = os.path.join("library", "commands", modname).replace(os.sep, ".")
+            mod = importlib.import_module(modpath)
+
+            if hasattr(mod, "group") and name == modname:
+                return mod if isinstance(mod.group, CommandWrapper) else None
+
+
+def get_commands() -> List[str]:
+    """get command names from library"""
+    cmds = []
+
+    for file in os.listdir("library/commands"):
+        if file.endswith(".py") and os.path.isfile("library/commands/" + file):
+            modname = file.rsplit(".py", 1)[0]
+            mod = load_command(modname)
+
+            if mod:
+                cmds.append(modname)
+
+    return cmds
